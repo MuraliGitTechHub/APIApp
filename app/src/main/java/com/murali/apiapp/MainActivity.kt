f8f0aedd2dcad223656a9858b6a66804b92d1f39
Muralidhar barik <muralidharbarik882@gmail.com>
2023-12-14 20:13:51 +0530

    API Call using Dagger 2 in android (Retrofit,MVVM,RecyclerView)

--- app/src/main/java/com/murali/apiapp/MainActivity.kt
+++ app/src/main/java/com/murali/apiapp/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.murali.apiapp
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
